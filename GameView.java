4d216e5b18d240b1184cba4d5552516d28c9d2f7
Ezard <ezard.ben@gmail.com>
2015-09-01 14:39:50 +0100

    Removed some uneeded comments

--- GameView.java
+++ GameView.java
@@ -31,42 +31,6 @@ public class GameView extends GLSurfaceView {
 		mRenderer.onResume();
 	}
 
-//	@Override
-//	public boolean onTouchEvent(MotionEvent event) {
-//		int index;
-//		if (event.getPointerCount() >= 2) {
-//			index = event.getActionIndex();
-//		} else {
-//			index = 0;
-//		}
-//		switch (event.getActionMasked()) {
-//			case MotionEvent.ACTION_DOWN:
-//			case MotionEvent.ACTION_POINTER_DOWN:
-//				if (event.getX(index) > getWidth() / 2) {
-//					Input.jump = true;
-//					return true;
-//				}
-//				break;
-//			case MotionEvent.ACTION_MOVE:
-//				if (event.getX(index) < getWidth() / 2f) {
-//					int x;
-//					if (event.getX(index) < getWidth() / 8f) {
-//						x = -1;
-//					} else {
-//						x = 1;
-//					}
-//					Input.set(x, 0);
-//				}
-//				break;
-//			case MotionEvent.ACTION_UP:
-//			case MotionEvent.ACTION_POINTER_UP:
-//				Input.set(0, 0);
-//				Input.jump = false;
-//				break;
-//		}
-//		return true;
-//	}
-
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		for (Input.OnTouchListener otl : Input.onTouchListeners) {
